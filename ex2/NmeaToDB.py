2b8243921c569f3929d1f24b76e98576164d02c8
Mesfin <Mesfin@DESKTOP-MCDQDAK>
2016-04-27 04:20:13 +0300

    speed,time,date work

--- ex2/NmeaToDB.py
+++ ex2/NmeaToDB.py
@@ -3,12 +3,11 @@ import csv
 import math
 import pymysql
 from datetime import datetime
-INPUT_FILENAME = "Nmea_Files/AttoPilot_Flight.txt"
-OUTPUT_FILENAME = 'Csv_Files/'+INPUT_FILENAME[11:-4]+'.csv'
+INPUT_FILENAME = "Nmea_Files/stockholm_walk.txt"
 
 with open(INPUT_FILENAME, 'r') as input_file:
     reader = csv.reader(input_file)
-    conn = pymysql.connect(host='localhost', port=3306, user='root', passwd='da9352238g', db='ex2')
+    conn = pymysql.connect(host='localhost', port=3306, user='root', passwd='Mes307Fin', db='ex2')
     c = conn.cursor()
     c.execute('DROP TABLE IF EXISTS nmea')
     #flag will tell us if the GPGGA is good if yes continue to the GPRMC
@@ -16,10 +15,10 @@ with open(INPUT_FILENAME, 'r') as input_file:
     # Create table
    # c.execute('''create table ex2.db.ex
                #(date text,time text,speed float, latitude text, latitude_direction text, longitude text, longitude_direction text,fix text,horizontal_dilution text,altitude text,direct_of_altitude text,altitude_location text)''')
-    # create a csv reader object from the input file (nmea files are basically csv)
+    # create a csv reader object from the input file nmea 
     
     c.execute('''create table nmea
-             (date DATE,time TIME,speed float, latitude text, longitude text)''')
+             (date DATE,time TIME,speed float , latitude text, longitude text)''')
     # create a csv reader object from the input file (nmea files are basically csv)
    
     for row in reader:
@@ -29,7 +28,6 @@ with open(INPUT_FILENAME, 'r') as input_file:
                 continue
 
             else:
-
                 # for each row, fetch the values from the row's columns
                 # columns that are not used contain technical GPS stuff that you are probably not interested in
                 time = row[1]
@@ -40,15 +38,27 @@ with open(INPUT_FILENAME, 'r') as input_file:
                 lon_direction = row[6]
                 speed = row[7]
                 date =  row[9]
-                 
-                tdate=datetime.strptime(date , '%d%m%y')
-                tdate=tdate.strftime('%y-%m-%d')[:] 
-                 
+            
+                #find time in format yyyy-mm-dd 
+                yyyy=date[4:]
+                mm=date[2:4]
+                dd=date[0:2]
+                tdate='%s-%s-%s' % (yyyy, mm, dd)
+             
+                #find latitude degree and latitude minute
+                latDegrees=latitude[0:2]
+                latMinute=latitude[2:]
                 
-                
-                tTime=datetime.strptime(time, '%H%M%S.%f')
-                tTime=tTime.strftime('%H:%M:%S.%f')[:-7] 
-        
+                #find latitude degree and latitude minute
+                lonDegrees=longitude[0:3]
+                lonMinute=longitude[3:]
+           
+                #find time in format hh:mm:ss
+                hours = time[0:2]
+                minutes = time[2:4]
+                seconds = time[4:6]
+          
+                tTime='%s:%s:%s' % (hours, minutes, seconds)
                 
                 # if the "warning" value is "V" (void), you may want to skip it since this is an indicator for an incomplete data row)
                 if warning == 'V':
@@ -65,28 +75,33 @@ with open(INPUT_FILENAME, 'r') as input_file:
                 # the following reflects this formula in mathematical terms.
                 # lat and lon have to be converted from string to float in order to do calculations with them.
                 # you probably want the values rounded to 6 digits after the point for better readability.
-                latitude = round(math.floor(float(latitude) / 100) + (float(latitude) % 100) / 60, 6)
+                #latitude = round(math.floor(float(latitude) / 100) + (float(latitude) % 100) / 60, 6)
+                latitude=round(float(latDegrees)+float(latMinute)/60,6)
+                 
                 if lat_direction == 'S':
                     latitude = latitude * -1
 
-                longitude = round(math.floor(float(longitude) / 100) + (float(longitude) % 100) / 60, 6)
+                #longitude = round(math.floor(float(longitude) / 100) + (float(longitude) % 100) / 60, 6)
+                longitude=round(float(lonDegrees)+float(lonMinute)/60,6)
+              
                 if lon_direction == 'W':
                     longitude = longitude * -1
 
                 # speed is given in knots, you'll probably rather want it in km/h and rounded to full integer values.
                 # speed has to be converted from string to float first in order to do calculations with it.
                 # conversion to int is to get rid of the tailing ".0".
-              
-                speed = int(round(float(speed) * 1.852, 0))
+               
+                if speed[0:] == '':
+                    speed=0
+                else:
+                    speed = int(float(speed) * 1.852)
 
                 # write the calculated/formatted values of the row that we just read into the csv file
-                c.execute("insert into nmea values (%s,%s,%s,%s,%s)",(date,time,speed, latitude, longitude))
-                
-
-        
+                c.execute("insert into nmea values (%s,%s,%s,%s,%s)",(tdate,tTime,speed, latitude, longitude))
        
-    conn.commit()   
-        
+    conn.commit() 
+    c.close()
+input_file.close()  
 # We can also close the connection if we are done with it.
 # Just be sure any changes have been committed or they will be lost.
 
--- ex2/nmea_to_csv.py
+++ ex2/nmea_to_csv.py
@@ -7,7 +7,7 @@ INPUT_FILENAME = "Nmea_Files/AttoPilot_Flight.txt"
 OUTPUT_FILENAME = 'Csv_Files/'+INPUT_FILENAME[11:-4]+'.csv'
 dbQuery='SELECT * FROM ex2.nmea;'
 
-db = pymysql.connect(host='localhost', port=3306, user='root', passwd='enter your sql password', db='ex2')
+db = pymysql.connect(host='localhost', port=3306, user='root', passwd='Mes307Fin', db='ex2')
 
 cur=db.cursor()
 cur.execute(dbQuery)
