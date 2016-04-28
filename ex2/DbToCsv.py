a9b1ebc60592e4b0e49440ea3c6a63029d413938
NaorT <naortamir4@gmail.com>
2016-04-28 18:30:52 +0300

    finish!!

--- ex2/DbToCsv.py
+++ ex2/DbToCsv.py
@@ -5,22 +5,24 @@ import pymysql
 
 INPUT_FILENAME = "Nmea_Files/running.txt"
 OUTPUT_FILENAME = 'Csv_Files/'+INPUT_FILENAME[11:-4]+'.csv'
-
-db = pymysql.connect(host='localhost', port=3306, user='root', passwd='123qwe', db='ex2')
-dbQuery='SELECT * FROM ex2.nmea;'
-cur=db.cursor()
-cur.execute(dbQuery)
-result=cur.fetchall()
-
-# create a csv writer object for the output file and write the header
 try:
+    db = pymysql.connect(host='localhost', port=3306, user='root', passwd='123qwe', db='ex2')
+
+    dbQuery='SELECT * FROM ex2.nmea;'
+    cur=db.cursor()
+    cur.execute(dbQuery)
+    result=cur.fetchall()
+       
+# create a csv writer object for the output file and write the header
+
     c = csv.writer(open(OUTPUT_FILENAME, 'w'))
     c.writerow(['  date','  time', '  speed', '  latitude', '  longitude'])
     for row in result:
         c.writerow(row)
-except IOError:
-    print ("Could not open file! Please close Excel!")
+    cur.close()
+    db.close()
+except :
+    print ("please chek if the file is already open or if your MySql details are correct")
 
-cur.close()
-db.close()
+
      
--- ex2/DbToKml.py
+++ ex2/DbToKml.py
@@ -27,24 +27,28 @@ kml_end = '''</coordinates></LineString>
 
 INPUT_FILENAME = "Nmea_Files/running.txt"
 OUTPUT_FILENAME = 'Kml_Files/'+INPUT_FILENAME[11:-4]+'.kml'
-db = pymysql.connect(host='localhost', port=3306, user='root', passwd='123qwe', db='ex2')
-dbQuery='SELECT * FROM ex2.nmea;'
-cur=db.cursor()
-cur.execute(dbQuery)
-result=cur.fetchall()
-f= open(OUTPUT_FILENAME,'w')
-f.write(kml_start)
-for row in result:
+try:
+    db = pymysql.connect(host='localhost', port=3306, user='root', passwd='123qwe', db='ex2')
+    dbQuery='SELECT * FROM ex2.nmea;'
+    cur=db.cursor()
+    cur.execute(dbQuery)
+    result=cur.fetchall()
+    f= open(OUTPUT_FILENAME,'w')
+    f.write(kml_start)
+    for row in result:
     
-    date=row[0] 
-    time=row[1]
-    speed=row[2]
-    lat=row[3]
-    lon=row[4]
-    f.write('%s,%s'%(lon,lat)+" ")
+        date=row[0] 
+        time=row[1]
+        speed=row[2]
+        lat=row[3]
+        lon=row[4]
+        f.write('%s,%s'%(lon,lat)+" ")
 
-f.write(kml_end)
-f.close()
+    f.write(kml_end)
+    f.close()
+except :
+    print ("please chek if your MySql details are correct")
+    
 
 
 
