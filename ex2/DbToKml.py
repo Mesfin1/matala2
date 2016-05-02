efba2fed030b4baf441648940e44ca2fac8c42fa
Mesfin <Mesfin@DESKTOP-MCDQDAK>
2016-05-02 22:11:06 +0300

    added read me file

--- ex2/DbToKml.py
+++ ex2/DbToKml.py
@@ -1,31 +1,28 @@
 import sys
 import pymysql
 from _datetime import datetime
-kml_start = '''<?xml version="1.0" encoding="UTF-8"?>
-<kml xmlns="http://www.opengis.net/kml/2.2"
- xmlns:gx="http://www.google.com/kml/ext/2.2">
-<Document>
-<name>LineStyle.kml</name>
-  <open>1</open>
+
+kml_start='''<?xml version="1.0" encoding="UTF-8"?>
+<kml xmlns="http://www.opengis.net/kml/2.2">
+ <Document>
   <Style id="linestyleExample">
     <LineStyle>
-      <color>7f0000ff</color>
+      <color name="orangered">#FF4500</color>
       <width>5</width>
       <gx:labelVisibility>2</gx:labelVisibility>
     </LineStyle>
   </Style>
   <Placemark>
-    <name>Route</name>
-    <styleUrl>#roadStyle</styleUrl>
-    <MultiGeometry>
-      <LineString><coordinates>'''
+    <LineString>
+     <altitudeMode>absolute</altitudeMode>
+      <coordinates>\n'''
 
-kml_end = '''</coordinates></LineString>
-    </MultiGeometry>
+kml_end=''' </coordinates>
+    </LineString>
   </Placemark>
-</Document>
-</kml>'''
-
+ </Document>
+</kml>
+'''
 
 INPUT_FILENAME = "Nmea_Files/Ariel2.txt"
 OUTPUT_FILENAME = 'Kml_Files/'+INPUT_FILENAME[11:-4]+'.kml'
@@ -44,7 +41,7 @@ f.write(kml_start)
 for row in result:
     lat=row[3]
     lon=row[5]
-    f.write('%s,%s'%(lon,lat)+" ")
+    f.write('%s,%s\n'%(lon,lat)+" ")
     
 f.write(kml_end)    
 f.close()
